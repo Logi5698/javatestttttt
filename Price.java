45e8270b985ced2a95f1f9169976d242c5370e6e
Logi5698 <79348695+Logi5698@users.noreply.github.com>
2023-05-09 23:15:52 +0800

    Update Price.java

--- Price.java
+++ Price.java
@@ -1,1 +1,27 @@
-Insert ur penis here
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package moonrailsystem;
+
+
+public class Price {
+    private double basePrice;
+    private String seatType;
+    private double priceMultiplier;
+
+    public Price(double basePrice, String seatType, double priceMultiplier) {
+        this.basePrice = basePrice;
+        this.seatType = seatType;
+        this.priceMultiplier = priceMultiplier;
+    }
+
+    public double calculatePrice() {
+        return basePrice * priceMultiplier;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+}
+
